b83565ec4283505c6cf3b2a503da1367fc176b6f
Ontan8 <tanishqpawar80@gmail.com>
2022-01-12 18:52:00 +0530

    created seperate widget for the transactions

---
+++ lib/widgets/transactions_list.dart
@@ -0,0 +1,19 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+
+class TransactionsList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return TransactionsListState();
+  }
+}
+
+class TransactionsListState extends State<TransactionsList> {
+  @override
+  Widget build(BuildContext context) {
+    return;
+  }
+}
