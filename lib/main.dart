a795dd7dfb29c2d74b4dc1e5f0e604d8a2ff0f33
Ontan8 <tanishqpawar80@gmail.com>
2022-01-12 19:24:25 +0530

    reduced widget tree density by segregation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './widgets/transactions_list.dart';
+import './widgets/inputForm.dart';
 
 //ignore_for_file: prefer_const_constructors
 //ignore_for_file: use_key_in_widget_constructors
@@ -42,37 +43,7 @@ class HomePage extends StatelessWidget {
             color: Colors.blue,
           ),
           Card(
-            child: Container(
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Title',
-                    ),
-                    controller: textController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Amount',
-                    ),
-                    controller: amountController,
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(10, 20, 10, 0),
-                    child: TextButton(
-                      onPressed: null,
-                      child: Text('Add Transaction'),
-                      style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.all(Colors.purple)),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            child: InputForm(),
           ),
           TransactionsList(),
         ],
