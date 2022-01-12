953b8d14b0012576b1b0411637d2166eaf1f4925
Ontan8 <tanishqpawar80@gmail.com>
2022-01-12 22:26:26 +0530

    changed file name

---
+++ lib/widgets/input_form.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+//ignore_for_file: prefer_const_constructors
+class InputForm extends StatelessWidget {
+  String? textVal;
+  double? amountVal;
+  final textController = TextEditingController();
+  final amountController = TextEditingController();
+  final Function funcc;
+  InputForm(this.funcc, {Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Title',
+            ),
+            controller: textController,
+            onChanged: (val) {
+              textVal = val;
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Amount',
+            ),
+            onChanged: (double val) {
+              amountVal = val;
+            },
+            controller: amountController,
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: TextButton(
+              onPressed: funcc(textVal, amountVal),
+              child: Text('Add Transaction'),
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.purple)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
