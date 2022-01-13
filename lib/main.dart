dd6eedbcfce6a60867f32a90210eb15511a90ce2
Ontan8 <tanishqpawar80@gmail.com>
2022-01-13 19:54:28 +0530

    added icon buttons and floating action buttons

--- lib/main.dart
+++ lib/main.dart
@@ -23,28 +23,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Expense Planner'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Card(
-            child: Container(
-              child: Text(
-                'Chart',
-                textAlign: TextAlign.center,
+        appBar: AppBar(
+          title: Text('Expense Planner'),
+          actions: const [
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.add),
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Card(
+                child: Container(
+                  child: Text(
+                    'Chart',
+                    textAlign: TextAlign.center,
+                  ),
+                  width: double.infinity,
+                  padding: EdgeInsets.all(10),
+                ),
+                elevation: 10,
+                color: Colors.blue,
               ),
-              width: double.infinity,
-              padding: EdgeInsets.all(10),
-            ),
-            elevation: 10,
-            color: Colors.blue,
+              UserTransactions(),
+            ],
           ),
-          UserTransactions(),
-        ],
-      ),
-    );
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ));
   }
 }
