a1b30321c4f649c496270dd591c7eb6e77dd5816
Ontan8 <tanishqpawar80@gmail.com>
2022-01-13 19:56:03 +0530

    onSubmitted function for text fields

--- lib/widgets/input_form.dart
+++ lib/widgets/input_form.dart
@@ -5,6 +5,14 @@ class InputForm extends StatelessWidget {
   final textController = TextEditingController();
   final amountController = TextEditingController();
   final Function funcc;
+  void sendData() {
+    if (textController.text.isEmpty ||
+        double.parse(amountController.text) <= 0) {
+      return;
+    }
+    funcc(textController.text, double.parse(amountController.text));
+  }
+
   InputForm(this.funcc, {Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -19,18 +27,21 @@ class InputForm extends StatelessWidget {
               labelText: 'Title',
             ),
             controller: textController,
+            keyboardType: TextInputType.text,
+            onSubmitted: (_) => sendData(),
           ),
           TextField(
             decoration: InputDecoration(
               labelText: 'Amount',
             ),
             controller: amountController,
+            keyboardType: TextInputType.number,
+            onSubmitted: (_) => sendData(),
           ),
           Container(
             margin: EdgeInsets.fromLTRB(10, 20, 10, 0),
             child: TextButton(
-              onPressed: () => funcc(
-                  textController.text, double.parse(amountController.text)),
+              onPressed: () => sendData(),
               child: Text('Add Transaction'),
               style: ButtonStyle(
                   foregroundColor: MaterialStateProperty.all(Colors.purple)),
