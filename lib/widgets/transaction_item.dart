ce4b40a5dee8627c3ae186e5fec4b73f989b13ef
Ontan8 <tanishqpawar80@gmail.com>
2022-01-19 18:26:30 +0530

    seperated widget from transaction_list

---
+++ lib/widgets/transaction_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({
+    Key? key,
+    required this.transaction,
+    required this.deleteTransaction,
+  }) : super(key: key);
+
+  final Transaction transaction;
+  final Function deleteTransaction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: FittedBox(child: Text(transaction.amount.toString())),
+          ),
+        ),
+        title: Text(
+          transaction.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        subtitle: Text(DateFormat.yMMMd().format(transaction.date)),
+        trailing: MediaQuery.of(context).size.width > 400
+            ? TextButton.icon(
+                icon: const Icon(Icons.delete),
+                onPressed: () => deleteTransaction(transaction.id),
+                label: const Text('Delete'),
+                style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all(
+                        Theme.of(context).primaryColor)),
+              )
+            : IconButton(
+                icon: const Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () {
+                  deleteTransaction(transaction.id);
+                },
+              ),
+      ),
+    );
+  }
+}
